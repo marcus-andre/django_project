f7d55fbe3e7e86d66ec4b444a359c4589f4d146e
marcus-andre <marcus-andre@hotmail.com>
2025-12-13 14:03:01 -0300

    Adjust the view in the hello_worl app to check type of request method

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+# What django.http does?
 # recebe a requisição HTTP (GET, POST, etc.) do navegador do usuário.
 # Ele pega todos os dados dessa requisição (cabeçalhos, dados de formulário, cookies, etc.) e os empacota em um objeto Python chamado HttpRequest
 # O URL Dispatcher (urls.py) pega este objeto HttpRequest e o passa como o argumento request para a sua função index: def index(request)
 # O Django fez todo o trabalho de parsing! Você só precisa usar o objeto request se precisar de dados dele.
 # Para que o Django saiba como empacotar sua resposta para o navegador, você usa o objeto HttpResponse importado de django.http.
 
+# Create your views here.
+
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
